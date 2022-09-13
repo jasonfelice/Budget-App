955d416be4a5e83dc8c3f8b55cf6a65a3c8eb218
Jake Felice <jasonfelice0@gmail.com>
2022-09-13 02:02:23 -0700

    Fix rubocop offenses

--- spec/models/category_sepc.rb
+++ spec/models/category_sepc.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Category, type: :model do
   before :each do
-    @user = User.create(name: 'Test', email: "test@example.com", password: 123456, password_confirmation: 123456)
+    @user = User.create(name: 'Test', email: 'test@example.com', password: 123_456, password_confirmation: 123_456)
   end
   it 'should be invalid without the icon' do
     category = Category.create(name: 'Test', icon: nil, user_id: @user.id)
