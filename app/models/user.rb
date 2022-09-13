b782d23fcd374a006c1f3ee2ff41fcf40bd76221
Jake Felice <jasonfelice0@gmail.com>
2022-09-13 01:29:49 -0700

    Add validations and relationship and User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 150 }
+  has_many :categories, dependent: :destroy
+  has_many :records, dependent: :destroy
 end
