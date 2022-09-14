4575bb75d4658afbd5d5dca7b18b323e592f89b9
Jake Felice <jasonfelice0@gmail.com>
2022-09-14 00:54:04 -0700

    Set up controllers

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,7 @@
+class CategoryController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+end
---
+++ app/controllers/records_controller.rb
@@ -0,0 +1,7 @@
+class RecordsController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+end
