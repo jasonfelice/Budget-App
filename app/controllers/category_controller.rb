f3166a9d82fd55f061ea5b091c03d7f24a755ef5
Jake Felice <jasonfelice0@gmail.com>
2022-09-15 01:31:56 -0700

    Create category_controller methods and creation form

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,7 +1,25 @@
 class CategoryController < ApplicationController
   def index; end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def create; end
+  def create
+    @user = current_user
+    @category = @user.categories.new(category_params)
+    if @@category.save
+      flash[:notice] = "#{@category.name} created successfully!"
+      redirect_to category_index_path
+    else
+      flash[:alert] = @category.errors.full_messages.first
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
