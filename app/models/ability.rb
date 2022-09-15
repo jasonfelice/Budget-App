545749a1b93e9124cd843fc9c10ae8ba91df34a1
Jake Felice <jasonfelice0@gmail.com>
2022-09-15 02:16:17 -0700

    Set up CanCanCan

---
+++ app/models/ability.rb
@@ -0,0 +1,9 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+      can :manage, Group, user: user
+      can :manage, Entity, user: user
+  end
+end
