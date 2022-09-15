9296820f8009d9b7803aa89a3780e8af9308c68b
Jake Felice <jasonfelice0@gmail.com>
2022-09-15 07:08:16 -0700

    Create records_controller methods

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,35 @@
 class RecordsController < ApplicationController
-  def index; end
+  def index
+    @category = current_user.categories.find(params[:category_id])
+    @records = @category.records
+  end
 
-  def new; end
+  def new
+    @category = current_user.categories.find(params[:category_id])
+    @record = @category.records.new
+  end
 
-  def create; end
+  def create
+    @category = current_user.categories.find(params[:category_id])
+    @record = current_user.records.new(record_params)
+    if @record.save
+      cateogry_record = @record.category_records.create(category_id: @category.id, record_id: @record_id)
+      if cateogry_record.save
+        flash.now[:notice] = "#{@record.name} created successfully!"
+        redirect_to category_records_path(@category)
+      else
+        flash.now[:alert] = "Something went wrong!"
+        render :new
+      end
+    else
+      flash.now[:alert] = @record.errors.full_messages.first
+      render :new
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:name, :amount)
+  end
 end
